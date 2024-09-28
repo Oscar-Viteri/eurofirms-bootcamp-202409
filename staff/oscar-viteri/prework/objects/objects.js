d623503ce0b5e69f7c6ed7abf089e2c27e6acb84
Oscar Viteri <oscar.viteri7@hotmail.com>
2024-09-28 09:06:06 +0200

    objects file #32

---
+++ staff/oscar-viteri/prework/objects/objects.js
@@ -0,0 +1,19 @@
+var n = 1
+var s = "hola mundo"
+b = true
+o = []
+
+o[0] = 100
+o[1] = 200
+o[2] = 300
+o[3] = 400
+o[10] = 1100
+o.name = "Oswald"
+o.surname = "Perez"
+o.lenght = 11
+
+for (var i = 0; i < o.length; i = i + 1) {
+    var element = o[i]
+    console.log(element)
+
+}
